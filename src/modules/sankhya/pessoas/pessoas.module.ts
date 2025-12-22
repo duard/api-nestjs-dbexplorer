6d50873b38d65250bcd82f525a1d5dfc511f7030
Carlos Developer <duard.developer@gmail.com>
2025-12-22 15:12:32 -0300

    carlos aquino - primeira marcha

--- src/modules/sankhya/pessoas/pessoas.module.ts
+++ src/modules/sankhya/pessoas/pessoas.module.ts
@@ -3,6 +3,7 @@ import { SqlServerService } from 'src/database/sqlserver.service';
 import { PessoasController } from './controllers/pessoas.controller';
 import { PessoasService } from './services/pessoas.service';
 
+
 @Module({
   controllers: [PessoasController],
   providers: [PessoasService, SqlServerService],
