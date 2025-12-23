5511529de363912c25c323149a564770b2d7df66
Carlos Developer <duard.developer@gmail.com>
2025-12-23 15:56:54 -0300

    carlos aquino - primeira marcha

--- src/modules/sankhya/tfpfun/tfpfun.module.ts
+++ src/modules/sankhya/tfpfun/tfpfun.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TFPFUNController } from './controllers/tfpfun.controller';
 import { TFPFUNService } from './services/tfpfun.service';
 
+import { SqlServerService } from 'src/database/sqlserver.service';
+
 @Module({
   controllers: [TFPFUNController],
-  providers: [TFPFUNService],
+  providers: [TFPFUNService, SqlServerService],
 })
 export class TFPFUNModule {}
